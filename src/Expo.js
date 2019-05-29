0e427ada91ab94fd124f80252738d50446a4d5be
Nicolas Righetti <nicolasrighetti@MacBook-Pro-de-Nicolas.local>
2019-05-29 21:00:04 +0200

    shuffle on generation

--- src/Expo.js
+++ src/Expo.js
@@ -28,8 +28,24 @@ class Expo extends React.Component {
     fetch("images.json")
       .then(res => res.json())
       .then(data => {
+        const array = [];
+        data.forEach(element => array.push(element));
+        let currentIndex = array.length, temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+          // Pick a remaining element...
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+
+          // And swap it with the current element.
+          temporaryValue = array[currentIndex];
+          array[currentIndex] = array[randomIndex];
+          array[randomIndex] = temporaryValue;
+        }
         this.setState({
-          pictures: data
+          pictures: array
         });
       });
   }
